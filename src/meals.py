b3fa806c04a6460a8316e21b84dd1d558a978038
cherry <cherryishairy@gmail.com>
2024-12-28 19:02:46 +1100

    removed holidays variable from meals.py

--- src/meals.py
+++ src/meals.py
@@ -12,19 +12,6 @@ MEALS = {
     ]
 }
 
-HOLIDAYS = [
-    "2024-01-01", #NY day
-    "2024-01-27", #aus day
-    "2024-04-18", #good fri
-    "2024-04-21", #easter mon
-    "2024-04-25", #anzac day
-    "2024-06-09", #king's bday
-    "2024-10-06", #labour day
-    "2024-12-25", #christmas
-    "2024-12-26", #boxing day
-]
-
-
 # constant parts that go on every menu
 SIDES = { 
     "zh": "水果，蔬菜",
